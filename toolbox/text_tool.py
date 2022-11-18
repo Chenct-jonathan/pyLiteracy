29097ddf6211210cbd17f4d3cce9ded54b161b60
Yu-Hsuan <58472945+Wu-Hsuan@users.noreply.github.com>
2022-11-18 22:22:13 +0800

    Update textpurger
    Delete the part of open files

--- toolbox/text_tool.py
+++ toolbox/text_tool.py
@@ -1,6 +1,5 @@
 
-def textpurger(filename):
-    with open (filename,"r", encoding = "utf-8") as f:
-        tagInputSTR = f.read()
-        tagInputSTR = tagInputSTR.replace(" ", "").replace("&gt","").replace("&lt","").replace("<s>","").replace("</s>","")
-    return tagInputSTR
+def textpurger(tagInputSTR):
+        rawInputSTR = tagInputSTR.replace(" ", "").replace("&gt","").replace("&lt","").replace("<s>","").replace("</s>","")
+    return rawInputSTR
+
