be2de17cec4990478e0bbb3a8576935f537a5bbe
Yu-Hsuan <58472945+Wu-Hsuan@users.noreply.github.com>
2022-11-18 22:24:22 +0800

    Update text_tool.py

--- toolbox/text_tool.py
+++ toolbox/text_tool.py
@@ -1,5 +1,5 @@
 
 def textpurger(tagInputSTR):
-        rawInputSTR = tagInputSTR.replace(" ", "").replace("&gt","").replace("&lt","").replace("<s>","").replace("</s>","")
+    rawInputSTR = tagInputSTR.replace(" ", "").replace("&gt","").replace("&lt","").replace("<s>","").replace("</s>","")
     return rawInputSTR
 
