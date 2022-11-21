cd6d42d28b06b2ed2d1ed14a9f54900684a7d8c8
joehuangx <joehuangx@gmail.com>
2022-11-22 00:03:25 +0800

    加上編碼檔頭

--- toolbox/text_tool.py
+++ toolbox/text_tool.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 def textpurger(tagInputSTR):
     rawInputSTR = tagInputSTR.replace(" ", "").replace("&gt","").replace("&lt","").replace("<s>","").replace("</s>","")
