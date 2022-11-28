f81a4382f80c34656bbed86daaabd591afff2680
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2022-11-29 00:53:29 +0800

    upload new purged corpus with phrase extraction

--- toolbox/corpus_pos.py
+++ toolbox/corpus_pos.py
@@ -4,33 +4,35 @@
 from text_tool import textPurger
 from ArticutAPI import Articut
 import json
+import re
 
 accountDICT = json.load(open("account.info",encoding="utf-8"))
 articut = Articut(username=accountDICT["username"],apikey=accountDICT["apikey"])
 
 def purge_pos_loc_zai(startLine, lastLine):
+    '''
+    input : raw corpus.txt
+    output : purged corpus + cws result.txt
+    startline : int
+    lastline : int 
+    '''
     purgedDICT = {}
     with open("../corpus/Sketch Engine 在.txt", encoding="utf-8") as f:
         lines = ''.join(f.readlines())
         rawLIST = textPurger(lines).split("\n")
         purgedLIST = []
         [purgedLIST.append(x) for x in rawLIST if x not in purgedLIST]
-        #print(purgedLIST)
     with open("../purged corpus/loc_zai_purged.txt", 'w', encoding = "utf-8") as g:
         for j in range(startLine, lastLine):#4539
-            #print(purgedLIST[j])
-            #purgedDICT['{}'.format(purgedLIST[j])] = ''.join(articut.parse(purgedLIST[j])['result_pos'])
-            #print(purgedDICT)
-            #g.write(''.join(articut.parse(purgedLIST[j])['result_pos'])+"\n")
-        #json.dump(purgedDICT,g, ensure_ascii=False)
-            g.write(purgedLIST[j]+"\n"+''.join(articut.parse(purgedLIST[j])['result_pos'])+"\n")
+            result_pos = articut.parse(purgedLIST[j],userDefinedDictFILE = "./USER_DEFINED.json")['result_pos']
+            for s in result_pos:
+                if "<FUNC_inner>在</FUNC_inner>" in s:
+                    #g.write(purgedLIST[j]+"\n"+''.join(articut.parse(purgedLIST[j])['result_pos'])+"\n")
+                    g.write("{}".format(re.sub('</?[a-zA-Z_]+>','',s))+"\n"+s+"\n")
             print(j)
-    #print(purgedDICT)
-
 
 def to_json(txtfile):
     zai_txtfile = open(txtfile, 'r', encoding='utf-8')
-
     count=0
     keys=[]
     values=[]
@@ -46,8 +48,6 @@ def to_json(txtfile):
         json.dump(dictionary, f, indent = 4, ensure_ascii=False)
 
 
-
-
 if __name__ == "__main__":
-    #purge_pos_loc_zai(5,4539)
+    #purge_pos_loc_zai(5,9222)
     to_json("../purged corpus/loc_zai_purged.txt")
