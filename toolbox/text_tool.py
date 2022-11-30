7427b1909c643c3bcb2e2eb62bb9241372aa6fae
Yu-Hsuan <58472945+Wu-Hsuan@users.noreply.github.com>
2022-11-30 09:39:53 +0800

    Update text_tool.py

--- toolbox/text_tool.py
+++ toolbox/text_tool.py
@@ -4,7 +4,6 @@
 def textPurger(tagInputSTR):
     '''
     Input:str (e.g. "<s> 碰到 無法 用 字面 解釋 的 句子 ， 你 真的 能 了解 美國人 在 說什麼嗎 ？ </s>" )
-
     Output:str (e.g. "碰到無法用字面解釋的句子，你真的能了解美國人在說什麼嗎 ？" )
     '''
     rawInputSTR = tagInputSTR.replace(" ", "").replace("&gt","").replace("&lt","").replace("<s>","").replace("</s>","")
@@ -13,7 +12,6 @@ def textPurger(tagInputSTR):
 def mergePurgedJson(jsonLIST,filename):
     '''
     Input:list (e.g. ["loc_zai_purged-10005.json","loc_zai_purged.json"]), str ("loc_zai_purged-all.json")
-    
     Output: json file
     '''
     resultJsonDICT = {}
