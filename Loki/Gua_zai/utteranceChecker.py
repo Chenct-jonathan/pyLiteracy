0145044744b941044ddbcfef9e16ac6bcaa5303a
joehuangx <joehuangx@gmail.com>
2022-12-10 09:15:25 +0800

    新增 missing zai (100-200)

--- Loki/Gua_zai/utteranceChecker.py
+++ Loki/Gua_zai/utteranceChecker.py
@@ -10,12 +10,12 @@ with open("loc_zai_purged.json", encoding="utf-8") as jFILE:
     corpusLIST = list(json.load(jFILE).keys())
 
 missingLIST = []
-for c in corpusLIST[:100]:
+for c in corpusLIST[100:200]:
     resultDICT = execLoki(c)
     if resultDICT["Zai"] == []:
         print("Missing pattern: {}".format(c))
         missingLIST.append(c)
 
 
-with open("missing_zai.json", "w", encoding="utf-8") as jFILE:
-    json.dump(missingLIST, jFILE, ensure_ascii=False)
+with open("missing_zai_100-200.json", "w", encoding="utf-8") as jFILE:
+    json.dump(missingLIST, jFILE, ensure_ascii=False, indent=4)
