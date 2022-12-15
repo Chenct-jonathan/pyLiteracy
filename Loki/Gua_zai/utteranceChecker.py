4be8da36c589de6b8bafd043c889ff5ee03068ff
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2022-12-15 18:07:05 +0800

    uploaded merged missing_zai

--- Loki/Gua_zai/utteranceChecker.py
+++ Loki/Gua_zai/utteranceChecker.py
@@ -11,11 +11,11 @@ with open("../../purged corpus/loc_zai_purged.json", encoding="utf-8") as jFILE:
 
 missingLIST = []
 
-for c in corpusLIST[100:200]:
+for c in corpusLIST[201:301]:
     resultDICT = execLoki(c)
     if resultDICT["Zai"] == []:
         print("Missing pattern: {}".format(c))
         missingLIST.append(c)
 
-with open("missing_zai_100-200.json", "w", encoding="utf-8") as jFILE:
+with open("missing_zai_201-300_revised.json", "w", encoding="utf-8") as jFILE:
     json.dump(missingLIST, jFILE, ensure_ascii=False, indent=4)
