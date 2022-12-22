26f52efd222f3ffbfe8494eb65aa0fcb73a6c856
joehuangx <joehuangx@gmail.com>
2022-12-22 23:14:11 +0800

    0-300

--- Loki/Gua_zai/utteranceChecker.py
+++ Loki/Gua_zai/utteranceChecker.py
@@ -11,11 +11,20 @@ with open("../../purged corpus/loc_zai_purged.json", encoding="utf-8") as jFILE:
 
 missingLIST = []
 
-for c in corpusLIST[201:301]:
+"""
+c = "111 終於在8月26日發出「中字第十二號」備忘錄"
+resultDICT = execLoki(c)
+if resultDICT["Zai"] == []:
+    print("hi")
+    print("Missing pattern: {}".format(c))
+    missingLIST.append(c)
+"""
+
+for c in corpusLIST[0:301]:
     resultDICT = execLoki(c)
     if resultDICT["Zai"] == []:
         print("Missing pattern: {}".format(c))
         missingLIST.append(c)
 
-with open("missing_zai_201-300_revised.json", "w", encoding="utf-8") as jFILE:
+with open("missing_zai_0-300_revised.json", "w", encoding="utf-8") as jFILE:
     json.dump(missingLIST, jFILE, ensure_ascii=False, indent=4)
