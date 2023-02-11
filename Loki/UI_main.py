f8a980e99c5a29a8086442296cbf27e8717af6d0
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2023-02-11 15:41:59 +0800

    add form

--- Loki/UI_main.py
+++ Loki/UI_main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def test():
-    return render_template("template/homepage.html")
+    return render_template("homepage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
