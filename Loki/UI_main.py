1f05bf7a8e8c6209aca92588dbdcc099688d6333
PeterWolf <peterwolf.wang@gmail.com>
2023-02-11 13:57:48 +0800

    增加網頁呈現

--- Loki/UI_main.py
+++ Loki/UI_main.py
@@ -3,12 +3,15 @@
 
 
 from flask import Flask
+from flask import render_template
+
+#from Gua_Zai import Gua_Zai.execLoki
 
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-    return "Hello World!"
+    return render_template("template/homepage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
